080eefe6102880247b3ece4aef6d9b26790d1ddb
vrodgreg <75463166+vrodgreg@users.noreply.github.com>
2021-02-19 17:24:21 -0500

    mybeers and wish list working

--- src/components/MyBeers.js
+++ src/components/MyBeers.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function MyBeers(props) {
+
+  let [beers, setBeers] = useState([]);
+  let beers2=[]
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://ironrest.herokuapp.com/beerdBeerList`
+      )
+      .then((res) => {
+        setBeers(res.data);
+        beers2=beers.data
+        console.log(typeof beers)
+      });
+  }, []);
+
+  const showBeers = () => {
+    return beers.map((eachBeer)=> {
+      return (
+        <Link to={`/AllBeers/${eachBeer.id}`}>
+          <div className="beerList">
+            <section>
+              <img id="beerListImage" src={eachBeer.image} height="150" alt="beer label" />
+            </section>
+
+            <section id="beerListName">
+              <h1>{eachBeer.name}</h1>
+            </section>
+          </div>
+
+        </Link>
+      )
+    })
+  };
+
+
   return (
     <div>
       <Link to="/">
@@ -9,6 +46,11 @@ function MyBeers(props) {
         <img id="beerHouse" src="/images/beerHome.png" alt="little home icon" />
         </header>
       </Link>
+
+      <div className="beerListTop">
+      <h1 id="beerListH1">My Beer Memories</h1>
+        <section className="allBeerList">{showBeers()}</section>
+      </div>
     </div>
   );
 }
